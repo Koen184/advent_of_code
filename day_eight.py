d9d3c2bbe8ac55668415ccfc15a190f8fc022a5f
Koen184 <136378440+Koen184@users.noreply.github.com>
2023-12-27 20:40:13 +0100

    Some work done (#7)
    * Finally 2nd part done !
    
    * Finally 2nd part done !
    
    * Day 9 done
    
    * Input for day 9

--- day_eight.py
+++ day_eight.py
@@ -1,4 +1,174 @@
+import re
+import math
+
+
 # DAY EIGHT
+
+# PART ONE
+
+# data = []
+#
+# with open('input_day8.txt') as file:
+#     for line in file:
+#         line = line.strip()
+#         data.append(line)
+#
+# instruction = data[0]
+# data = data[2:]
+#
+# mod_data = []
+#
+# for element in data:
+#     mod_data.append([element[0:3], element[7:10], element[12:15]])
+#
+# for data in mod_data:
+#     if data[0] == 'AAA':
+#         actual = data
+#     elif data[0] == 'ZZZ':
+#         end = data
+#
+# steps = 0
+# index = 0
+# flag = 0
+#
+# while flag == 0:
+#     if index == len(instruction):
+#         index = 0
+#
+#     for element in mod_data:
+#         if instruction[index] == 'R' and element[0] == actual[2]:
+#             actual = element
+#             steps = steps + 1
+#             index = index + 1
+#
+#             if actual[0] == end[0]:
+#                 flag = 1
+#
+#             break
+#
+#         elif instruction[index] == 'L' and element[0] == actual[1]:
+#             actual = element
+#             steps = steps + 1
+#             index = index + 1
+#
+#             if actual[0] == end[0]:
+#                 flag = 1
+#
+#             break
+#
+# print(steps)
+
+# PART TWO
+
+# data = []
+#
+# with open('input_day8.txt') as file:
+#     for line in file:
+#         line = line.strip()
+#         data.append(line)
+#
+# instruction = data[0]
+# data = data[2:]
+#
+# mod_data = []
+#
+# for element in data:
+#     mod_data.append([element[0:3], element[7:10], element[12:15]])
+#
+# actual = []
+# end = []
+#
+# for data in mod_data:
+#     if re.match(r'..A', data[0]):
+#         actual.append(data)
+#     elif re.match(r'..Z', data[0]):
+#         end.append(data)
+#
+# steps = 0
+# index = 0
+# flag = 0
+#
+# while flag == 0:
+#     if index == len(instruction):
+#         index = 0
+#
+#     for i, element_actual in enumerate(actual):
+#         for element in mod_data:
+#             if instruction[index] == 'R' and element[0] == element_actual[2]:
+#                 actual[i] = element
+#                 break
+#             elif instruction[index] == 'L' and element[0] == element_actual[1]:
+#                 actual[i] = element
+#                 break
+#
+#     steps = steps + 1
+#     index = index + 1
+#
+#     check = 0
+#
+#     for element_actual in actual:
+#         if re.match(r'..Z', element_actual[0]):
+#             check = check + 1
+#             print(check)
+#             print(steps)
+#
+#     if check == len(end):
+#         flag = 1
+#
+# print(steps)
+
+
+# PART TWO _ OPTION B -> faster
+
+# data = []
+#
+# with open('input_day8.txt') as file:
+#     for line in file:
+#         line = line.strip()
+#         data.append(line)
+#
+# instruction = data[0]
+# data = data[2:]
+#
+# data_dict = {}
+#
+# for element in data:
+#     key = element[0:3]
+#     data_dict[key] = (element[0:3], element[7:10], element[12:15])
+#
+# actual = set()
+# end = set()
+#
+# for key, value in data_dict.items():
+#     if key.endswith('A'):
+#         actual.add(key)
+#     elif key.endswith('Z'):
+#         end.add(key)
+#
+# steps = 0
+# index = 0
+#
+# while True:
+#     if index == len(instruction):
+#         print(steps)
+#         index = 0
+#
+#     for key in list(actual):
+#         next_key = data_dict[key][2] if instruction[index] == 'R' else data_dict[key][1]
+#         if next_key in data_dict:
+#             actual.remove(key)
+#             actual.add(next_key)
+#
+#     steps += 1
+#     index += 1
+#
+#     if sum(1 for key in actual if key.endswith('Z')) == len(end):
+#         break
+#
+# print(steps)
+
+
+# PART TWO - FINAL - least common multiple
 
 data = []
 
@@ -15,39 +185,42 @@ mod_data = []
 for element in data:
     mod_data.append([element[0:3], element[7:10], element[12:15]])
 
+actual = []
+end = []
+
 for data in mod_data:
-    if data[0] == 'AAA':
-        actual = data
-    elif data[0] == 'ZZZ':
-        end = data
+    if re.match(r'..A', data[0]):
+        actual.append(data)
+    elif re.match(r'..Z', data[0]):
+        end.append(data)
 
-steps = 0
-index = 0
-flag = 0
+lcm_values = []
 
-while flag == 0:
-    if index == len(instruction):
-        index = 0
+for element_actual in actual:
+    steps = 0
+    index = 0
+    flag = 0
 
-    for element in mod_data:
-        if instruction[index] == 'R' and element[0] == actual[2]:
-            actual = element
-            steps = steps + 1
-            index = index + 1
+    while flag == 0:
+        if index == len(instruction):
+            index = 0
 
-            if actual[0] == end[0]:
-                flag = 1
+        for element in mod_data:
+            if instruction[index] == 'R' and element[0] == element_actual[2]:
+                element_actual = element
+                break
+            elif instruction[index] == 'L' and element[0] == element_actual[1]:
+                element_actual = element
+                break
 
-            break
+        steps = steps + 1
+        index = index + 1
 
-        elif instruction[index] == 'L' and element[0] == actual[1]:
-            actual = element
-            steps = steps + 1
-            index = index + 1
+        check = 0
 
-            if actual[0] == end[0]:
-                flag = 1
+        if element_actual in end:
+            lcm_values.append(steps)
+            flag = 1
 
-            break
-
-print(steps)
+print(lcm_values)
+print(math.lcm(*lcm_values))
--- day_three.py
+++ day_three.py
@@ -3,62 +3,131 @@
 import numpy as np
 
 
+# FUNCTIONS - PART ONE
+
 def check_symbols(array, num, list_of_nums):
     size_clip = list(array.shape)
     elements = size_clip[0] * size_clip[1] - len(num)
 
     count_dots = np.count_nonzero(array == '.')
 
-    # print(elements, count_dots)
-    # print(array)
-
     if elements != count_dots:
         list_of_nums.append(int(num))
-        print('INPUT: ', num)
 
+
+# PART ONE
+
+# with open('input_day3.txt') as file:
+#     lines = file.readlines()
+#     lines = [line.strip() for line in lines]
+#     lines = [(line + '.') for line in lines]
+#     data = np.array([list(line) for line in lines])
+#
+# size = list(data.shape)
+# numbers = []
+#
+# for i in range(0, size[0]):
+#     number = ''
+#
+#     for j in range(0, size[1]):
+#         if data[i, j].isdigit():
+#             number = number + data[i, j]
+#         else:
+#             if not number == '':
+#                 if (j - len(number) - 1) <= 0 and (i - 1) <= 0 and number != '':
+#                     check_data = data[0:(i + 2), 0:(j + 1)]
+#                     check_symbols(check_data, number, numbers)
+#                     number = ''
+#                 elif (j - len(number) - 1) <= 0 and number != '':
+#                     check_data = data[(i - 1):(i + 2), 0:(j + 1)]
+#                     check_symbols(check_data, number, numbers)
+#                     number = ''
+#                 elif (i - 1) <= 0 and number != '':
+#                     check_data = data[0:(i + 2), (j - len(number) - 1):(j + 1)]
+#                     check_symbols(check_data, number, numbers)
+#                     number = ''
+#                 else:
+#                     check_data = data[(i - 1):(i + 2), (j - len(number) - 1):(j + 1)]
+#                     check_symbols(check_data, number, numbers)
+#                     number = ''
+#
+# print(sum(numbers))
+
+
+# FUNCTIONS - PART TWO
+
+
+def extract_numbers(array):
+    numbers = []
+    for row in array:
+        num_str = ''
+        for char in row:
+            if char.isdigit():
+                num_str += char
+            elif num_str:
+                numbers.append(int(num_str))
+                num_str = ''
+        if num_str:
+            numbers.append(int(num_str))
+    return numbers
+
+
+def check_numbers(array, ratios):
+    rows = []
+
+    for row in array:
+        rows.append(list(row))
+
+    for row in rows:
+        if row[2].isdigit() and row[4].isdigit():
+            if row[3].isdigit():
+                row[0] = '.'
+                row[6] = '.'
+            else:
+                continue
+        elif row[2].isdigit() and row[3].isdigit():
+            row[0] = '.'
+            row[5] = '.'
+            row[6] = '.'
+        elif row[2].isdigit():
+            row[5] = '.'
+            row[6] = '.'
+        elif row[4].isdigit() and row[3].isdigit():
+            row[0] = '.'
+            row[1] = '.'
+            row[6] = '.'
+        elif row[4].isdigit():
+            row[0] = '.'
+            row[1] = '.'
+        else:
+            row[0] = '.'
+            row[1] = '.'
+            row[5] = '.'
+            row[6] = '.'
+
+    numbers = extract_numbers(rows)
+
+    if len(numbers) != 2:
+        return
+    else:
+        ratios.append(numbers[0] * numbers[1])
+
+
+# PART TWO
 
 with open('input_day3.txt') as file:
     lines = file.readlines()
     lines = [line.strip() for line in lines]
+    lines = [('.' + line + '.') for line in lines]
     data = np.array([list(line) for line in lines])
 
-print(data)
-
 size = list(data.shape)
-numbers = []
+gear_ratios = []
 
 for i in range(0, size[0]):
-    number = ''
-
     for j in range(0, size[1]):
-        if data[i, j].isdigit():
-            number = number + data[i, j]
-            print(number)
-        else:
-            if not number == '':
-                if (j - len(number) - 1) <= 0 and (i - 1) <= 0 and number != '':
-                    check_data = data[0:(i + 2), 0:(j + 1)]
-                    check_symbols(check_data, number, numbers)
-                    number = ''
-                    print(check_data)
-                elif (j - len(number) - 1) <= 0 and number != '':
-                    check_data = data[(i - 1):(i + 2), 0:(j + 1)]
-                    check_symbols(check_data, number, numbers)
-                    number = ''
-                    print(check_data)
-                elif (i - 1) <= 0 and number != '':
-                    check_data = data[0:(i + 2), (j - len(number) - 1):(j + 1)]
-                    check_symbols(check_data, number, numbers)
-                    number = ''
-                    print(check_data)
-                else:
-                    check_data = data[(i - 1):(i + 2), (j - len(number) - 1):(j + 1)]
-                    check_symbols(check_data, number, numbers)
-                    number = ''
-                    print(check_data)
+        if data[i, j] == '*':
+            check_data = data[(i - 1):(i + 2), (j - 3):(j + 4)]
+            check_numbers(check_data, gear_ratios)
 
-
-print(numbers)
-print(sum(numbers))
-print(len(numbers))
-
+print(sum(gear_ratios))
